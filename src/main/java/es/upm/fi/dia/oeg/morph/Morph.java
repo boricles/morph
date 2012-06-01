4316a7581060ecc24311d7460e9f62656913bbf2
Boris Villazon-Terrazas <boricles@gmail.com>
2012-06-01 15:14:59 +0200

    Props file reader included

--- src/main/java/es/upm/fi/dia/oeg/morph/Morph.java
+++ src/main/java/es/upm/fi/dia/oeg/morph/Morph.java
@@ -1,14 +1,27 @@
 package es.upm.fi.dia.oeg.morph;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.logging.Level;
+
+import org.apache.log4j.Logger;
+
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.ResourceFactory;
+
 
 public class Morph 
 {
 	protected static final String uri="http://es.upm.fi.dia.oeg/morph#";
 	
 	public static final Property uniqueIndex = property("uniqueIndex");
+	
+	private static Logger logger = Logger.getLogger(Morph.class.getName());
 
 	public static String getUri()
 	{
@@ -25,8 +38,45 @@ public class Morph
 		return ResourceFactory.createProperty( uri, local ); 
 	}
 	
+	private static Properties buildConfiguration(String path) {
+	    Properties properties = new Properties();
+	    try {
+	      properties.load(new FileInputStream(path));
+	    } catch (IOException io) {
+	    	logger.warn("Problems loading configuration file: {0}",io);
+	    	System.exit(0);
+	    }
+	    return properties;
+	   
+	  }
+	
 	public static void main(String []args) {
-		System.out.println("Running");
+		try {
+			if (args.length==1) {  
+				String propertiesFilePath = args[0];
+				Properties props = buildConfiguration(propertiesFilePath);
+				R2RProcessor r2rprocessor = new R2RProcessor();
+				r2rprocessor.configure(props);
+				Dataset dataset = r2rprocessor.transform();
+			
+				//temporary code
+				Model m = dataset.getDefaultModel();
+				logger.info("Write default graph");
+				m.write(System.out, "TURTLE");
+				Iterator<String> names = dataset.listNames();
+				while (names.hasNext())
+				{
+					String uri = names.next();
+					logger.info("Write graph: "+uri );
+					Model nm = dataset.getNamedModel(uri);
+					nm.write(System.out,"TURTLE");
+				}	
+			}
+			else
+				logger.error("Incorrect number of arguments. Please specify the properties file location.");
+		}
+		catch(Exception ex) {
+			logger.error("Error during the configuration ", ex);
+		}
 	}
-
 }
